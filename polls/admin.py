928825b5eab84f383f472eab83afcb685ef96db3
Yogesh Kumar <yogeshkumar02691@gmail.com>
2020-03-24 13:41:57 +0530

    done some changes to admin.py file, models.py, index.html, tests.py, views.py and sdded some static files style.css and background.gif

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+class ChoiceInline(admin.StackedInline):
+        model=Choice
+        extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+	"""docstring for QuestionAdmin"""
+	fieldsets = [(None, {'fields':['question_text']}),
+                     ('Data information',{'fields':['pub_date'],'classes':['collapse']}),
+                     ]
+	inlines=[ChoiceInline]
+		
+
+admin.site.register(Question,QuestionAdmin)
